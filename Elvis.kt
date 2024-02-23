3af4936b25f6724b4993cc6fb414aee84aca0ba2
VivianeMeiraMarques <141928555+VivianeMeiraMarques@users.noreply.github.com>
2024-02-23 10:06:52 -0300

    Add files via upload

---
+++ Elvis.kt
@@ -0,0 +1,16 @@
+/**
+* Operador Elvis
+*/
+fun main() {
+    val str: String? = null
+    
+    // Verificação padrão
+    if (str == null) {
+        println("NULO TEXTO INFORMATIVO")
+    } else {
+        println(str)
+    }
+
+println(str ?: "NULO TEXTO INFORMATIVO")
+
+}
